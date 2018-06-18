b1d5ea5600a1d8fe4af8f9a46485876815a36010
Alex <alex@vaadin.com>
2018-06-18 22:40:02 +0300

    Remove uid from test names

--- vaadin-confirm-dialog-integration-tests/src/test/java/com/vaadin/flow/component/confirmdialog/test/FeaturesDiyIT.java
+++ vaadin-confirm-dialog-integration-tests/src/test/java/com/vaadin/flow/component/confirmdialog/test/FeaturesDiyIT.java
@@ -8,7 +8,9 @@ public class FeaturesDiyIT extends AbstractParallelTest {
 
 	public Actionwords actionwords = new Actionwords(this, FeaturesDiy.class);
 
-	public void reminderDialog(String sampleName) throws Exception {
+	@Test
+	public void testReminderDialog() throws Exception {
+		String sampleName = "SampleConfirmDialog";
 		actionwords.iHaveSampleDialog(sampleName);
 		actionwords.iOpenDialogDialog(sampleName);
 		actionwords.iCompareTheDialogToReferenceImage(sampleName);
@@ -18,29 +20,8 @@ public class FeaturesDiyIT extends AbstractParallelTest {
 	}
 
 	@Test
-	public void testReminderDialogBasicUid56bdf3b7f1a14bec8a6691ab5acd8c82() throws Exception {
-		reminderDialog("SampleConfirmDialog");
-	}
-
-	public void confirmDeleteDialog(String sampleName) throws Exception {
-		actionwords.iHaveSampleDialog(sampleName);
-		actionwords.iOpenDialogDialog(sampleName);
-		actionwords.iCompareTheDialogToReferenceImage(sampleName);
-		actionwords.iClickConfirm();
-		actionwords.dialogDialogIsClosed(sampleName);
-		actionwords.confirmEventIsFired();
-		actionwords.iOpenDialogDialog(sampleName);
-		actionwords.iClickCancel();
-		actionwords.dialogDialogIsClosed(sampleName);
-		actionwords.cancelEventIsFired();
-	}
-
-	@Test
-	public void testConfirmDeleteDialogBasicUid73a333b73ac94e83a079bdb9725411ce() throws Exception {
-		confirmDeleteDialog("SampleConfirmDeleteDialog");
-	}
-
-	public void areYouSureYouWantToPublishDialog(String sampleName) throws Exception {
+	public void testConfirmDeleteDialog() throws Exception {
+		String sampleName = "SampleConfirmDeleteDialog";
 		actionwords.iHaveSampleDialog(sampleName);
 		actionwords.iOpenDialogDialog(sampleName);
 		actionwords.iCompareTheDialogToReferenceImage(sampleName);
@@ -54,11 +35,23 @@ public class FeaturesDiyIT extends AbstractParallelTest {
 	}
 
 	@Test
-	public void testAreYouSureYouWantToPublishDialogBasicUid4eb6bd3dd8bc4324b05c1d96c4972ca8() throws Exception {
-		areYouSureYouWantToPublishDialog("SampleConfirmPublishDialog");
+	public void testAreYouSureYouWantToPublishDialog() throws Exception {
+		String sampleName = "SampleConfirmPublishDialog";
+		actionwords.iHaveSampleDialog(sampleName);
+		actionwords.iOpenDialogDialog(sampleName);
+		actionwords.iCompareTheDialogToReferenceImage(sampleName);
+		actionwords.iClickConfirm();
+		actionwords.dialogDialogIsClosed(sampleName);
+		actionwords.confirmEventIsFired();
+		actionwords.iOpenDialogDialog(sampleName);
+		actionwords.iClickCancel();
+		actionwords.dialogDialogIsClosed(sampleName);
+		actionwords.cancelEventIsFired();
 	}
 
-	public void unsavedChangesDialog(String sampleName) throws Exception {
+	@Test
+	public void testUnsavedChangesDialog() throws Exception {
+		String sampleName = "SampleUnsavedChangesDialog";
 		actionwords.iHaveSampleDialog(sampleName);
 		actionwords.iOpenDialogDialog(sampleName);
 		actionwords.iCompareTheDialogToReferenceImage(sampleName);
@@ -75,9 +68,4 @@ public class FeaturesDiyIT extends AbstractParallelTest {
 		actionwords.cancelEventIsFired();
 	}
 
-	@Test
-	public void testUnsavedChangesDialogBasicUid6064e2706c3d4e8993a17c07bcc49f7f() throws Exception {
-		unsavedChangesDialog("SampleUnsavedChangesDialog");
-	}
 }
-
