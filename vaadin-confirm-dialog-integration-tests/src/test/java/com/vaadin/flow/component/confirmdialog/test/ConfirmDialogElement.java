27538fab252d707580e63440967174e33e857307
Alex <alex@vaadin.com>
2018-06-19 11:03:12 +0300

    Use spaces instead of tabs

--- vaadin-confirm-dialog-integration-tests/src/test/java/com/vaadin/flow/component/confirmdialog/test/ConfirmDialogElement.java
+++ vaadin-confirm-dialog-integration-tests/src/test/java/com/vaadin/flow/component/confirmdialog/test/ConfirmDialogElement.java
@@ -8,29 +8,29 @@ import com.vaadin.testbench.elementsbase.Element;
 @Element("vaadin-confirm-dialog")
 public class ConfirmDialogElement extends TestBenchElement {
 
-	private TestBenchElement getOverlayContext() {
-		return $("vaadin-dialog-overlay").onPage().first().$(TestBenchElement.class).id("content");
-	}
+    private TestBenchElement getOverlayContext() {
+        return $("vaadin-dialog-overlay").onPage().first().$(TestBenchElement.class).id("content");
+    }
 
-	private TestBenchElement getButton(String buttonId, String slotName) {
-		ElementQuery<TestBenchElement> query = getOverlayContext().$(TestBenchElement.class)
-				.attribute("slot", slotName);
-		if (query.exists()) {
-			return query.first();
-		}
+    private TestBenchElement getButton(String buttonId, String slotName) {
+        ElementQuery<TestBenchElement> query = getOverlayContext().$(TestBenchElement.class)
+                .attribute("slot", slotName);
+        if (query.exists()) {
+            return query.first();
+        }
 
-		return getOverlayContext().$(ButtonElement.class).id(buttonId);
-	}
+        return getOverlayContext().$(ButtonElement.class).id(buttonId);
+    }
 
-	public TestBenchElement getConfirmButton() {
-		return getButton("confirm", "confirm-button");
-	}
+    public TestBenchElement getConfirmButton() {
+        return getButton("confirm", "confirm-button");
+    }
 
-	public TestBenchElement getRejectButton() {
-		return getButton("reject", "reject-button");
-	}
+    public TestBenchElement getRejectButton() {
+        return getButton("reject", "reject-button");
+    }
 
-	public TestBenchElement getCancelButton() {
-		return getButton("cancel", "cancel-button");
-	}
+    public TestBenchElement getCancelButton() {
+        return getButton("cancel", "cancel-button");
+    }
 }
