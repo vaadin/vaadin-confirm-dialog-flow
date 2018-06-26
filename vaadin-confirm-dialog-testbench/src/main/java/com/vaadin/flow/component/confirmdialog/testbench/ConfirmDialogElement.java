789f499281c144de03cedf80b0adbe6cf391ffec
Guillermo Alvarez <guillermo@vaadin.com>
2018-06-26 10:55:44 +0300

    Add module for testbench API

--- vaadin-confirm-dialog-testbench/src/main/java/com/vaadin/flow/component/confirmdialog/testbench/ConfirmDialogElement.java
+++ vaadin-confirm-dialog-testbench/src/main/java/com/vaadin/flow/component/confirmdialog/testbench/ConfirmDialogElement.java
@@ -1,4 +1,21 @@
-package com.vaadin.flow.component.confirmdialog.test;
+package com.vaadin.flow.component.confirmdialog.testbench;
+
+/*
+ * #%L
+ * Vaadin Confirm Dialog Testbench API
+ * %%
+ * Copyright (C) 2018 Vaadin Ltd
+ * %%
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
+ * 
+ * See the file license.html distributed with this software for more
+ * information about licensing.
+ * 
+ * You should have received a copy of the CVALv3 along with this program.
+ * If not, see <http://vaadin.com/license/cval-3>.
+ * #L%
+ */
 
 import com.vaadin.flow.component.button.testbench.ButtonElement;
 import com.vaadin.testbench.ElementQuery;
